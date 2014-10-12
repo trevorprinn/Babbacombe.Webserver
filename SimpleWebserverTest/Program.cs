a56a330abe3aae7ea11e40ac0e771421646c083c
Trevor Prinn <trev@babbacom.com>
2014-10-12 19:20:51 +0100

    Added simple test app.

---
+++ SimpleWebserverTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Babbacombe.Webserver;
+
+namespace SimpleWebserverTest {
+    class Program {
+        static void Main(string[] args) {
+            using (var ws = new HttpServer()) {
+                ws.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
