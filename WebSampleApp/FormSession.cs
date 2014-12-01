9a0a44ab559fea3b288cd777f172cc1e3628cb7f
Trevor Prinn <trev@babbacom.com>
2014-12-01 19:32:27 +0000

    Shows consecutive url count in session forms.

--- WebSampleApp/FormSession.cs
+++ WebSampleApp/FormSession.cs
@@ -11,6 +11,9 @@ using System.Windows.Forms;
 namespace Babbacombe.WebSampleApp.Exercise {
     partial class FormSession : Form {
         private HttpSession _session;
+        private string _lastUrl;
+        private string _lastMethod;
+        private int _lastUrlCount;
 
         public FormSession(HttpSession session) {
             InitializeComponent();
@@ -29,11 +32,22 @@ namespace Babbacombe.WebSampleApp.Exercise {
                 BeginInvoke(new Action<Uri, string>(AddUrl), url, method);
                 return;
             }
+            if (url.ToString() == _lastUrl && _lastMethod == method) {
+                var ix = listUrls.TopIndex;
+                listUrls.Items.RemoveAt(listUrls.Items.Count - 1);
+                listUrls.Items.Add(string.Format("{0} {1} {2}", url, method, ++_lastUrlCount));
+                listUrls.TopIndex = ix;
+                return;
+            }
+
             // Autoscroll the list box if it is showing the last url.
             int visibleCount = listUrls.ClientSize.Height / listUrls.ItemHeight;
             bool autoScroll = listUrls.TopIndex + visibleCount >= listUrls.Items.Count;
             listUrls.Items.Add(string.Format("{0} {1}", url, method));
             if (autoScroll) listUrls.TopIndex++;
+            _lastUrl = url.ToString();
+            _lastMethod = method;
+            _lastUrlCount = 1;
         }
 
         public IEnumerable<string> GetServerValues() {
